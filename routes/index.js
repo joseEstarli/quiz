a75b7b81796f4f1f4b354e165235580a80fc1957
joseEstarli <jose.estarli@gmail.com>
2015-07-13 10:51:04 +0200

    Cambios del enunciado P2P

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/author', function(req, res) {
+  res.render('author', { autor: 'Jos\u00E9 Estarl\u00ED' });
+});
+
+
 module.exports = router;
