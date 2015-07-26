6bd077347d5b2e9845a0cffce204ab9fa5d94296
Jose M Estarli <jose.estarli@gmail.com>
2015-07-26 15:15:27 +0200

    Quiz 13: editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,6 +38,7 @@ exports.show = function(req, res) {
 // GET /quizes/:id/answer
 exports.answer = function (req, res){
   models.Quiz.find(req.params.quizId).then(function(quiz) {
+    var resultado = 'Incorrecto';
     if (req.query.respuesta === quiz.respuesta) {
        resultado = 'Correcto';
      }
@@ -64,10 +65,7 @@ exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
 // guarda en DB los campos pregunta y respuesta de quiz
-  quiz
-  .validate()
-  .then(
-    function(err){
+  quiz.validate().then( function(err){
       if (err) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
@@ -80,3 +78,26 @@ exports.create = function(req, res) {
 };
 
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then( function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
+};
